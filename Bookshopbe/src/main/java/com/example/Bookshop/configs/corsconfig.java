7706bac65320a574b55dd222038939c86a1090f0
dung <vongocthanhjk@gmail.com>
2026-08-21 21:20:48 +0700

    fix corsconfig

--- Bookshopbe/src/main/java/com/example/Bookshop/configs/corsconfig.java
+++ Bookshopbe/src/main/java/com/example/Bookshop/configs/corsconfig.java
@@ -13,7 +13,7 @@ public class corsconfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174", "http://localhost:5173", "http://localhost:3000", "http://localhost:8080",  "https://bookshop-liard-eta.vercel.app")
+                        .allowedOrigins("http://localhost:5174", "http://localhost:5173", "http://localhost:3000", "http://localhost:8080",  "https://bookshop-liard-eta.vercel.app/api")
                         .allowedMethods(
                                 "GET",
                                 "POST",
